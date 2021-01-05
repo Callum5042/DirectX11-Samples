6df6d58ad4fbb9482401d8ebb6d337dd4b413ab7
Callum Anning <CallumAnning@hotmail.co.uk>
2021-01-05 19:56:49 +0000

    Added Skeletal Animation project

---
+++ Skeletal Animation/DxModel.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "DxRenderer.h"
+#include <vector>
+#include <DirectXColors.h>
+
+namespace DX
+{
+	struct Colour
+	{
+		float r = 0;
+		float g = 0;
+		float b = 0;
+		float a = 0;
+	};
+
+	struct Vertex
+	{
+		// Vertex position
+		float x = 0;
+		float y = 0;
+		float z = 0;
+
+		// Vertex colour
+		Colour colour = {};
+	};
+
+	class Model
+	{
+	public:
+		Model(DX::Renderer* renderer);
+		virtual ~Model() = default;
+
+		// Create device
+		void Create();
+
+		// Render the model
+		void Render();
+
+		// World 
+		DirectX::XMMATRIX World = DirectX::XMMatrixIdentity();
+
+	private:
+		DX::Renderer* m_DxRenderer = nullptr;
+
+		// Number of indices to draw
+		UINT m_IndexCount = 0;
+
+		// Vertex buffer
+		ComPtr<ID3D11Buffer> m_d3dVertexBuffer = nullptr;
+		void CreateVertexBuffer();
+
+		// Index buffer
+		ComPtr<ID3D11Buffer> m_d3dIndexBuffer = nullptr;
+		void CreateIndexBuffer();
+	};
+}
