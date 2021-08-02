f20dbd7e6f9191b384029a6a39c520d165feb66c
Callum Anning <CallumAnning@hotmail.co.uk>
2021-08-02 23:55:18 +0100

    Removed unused code

--- Sources/Point Sprite/PixelShader.hlsl
+++ Sources/Point Sprite/PixelShader.hlsl
@@ -3,6 +3,5 @@
 // Entry point for the vertex shader - will be executed for each pixel
 float4 main(PixelInput input) : SV_TARGET
 {
-	//float4 diffuse_texture = gTextureDiffuse.Sample(gSamplerAnisotropic, input.tex);
 	return float4(1.0f, 0.0f, 0.0f, 1.0f);
 }
