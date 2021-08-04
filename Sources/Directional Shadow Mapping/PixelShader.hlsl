830d0fb75158218afd27e3e75f3bbbb1f4ca3cdc
Callum Anning <CallumAnning@hotmail.co.uk>
2021-08-04 20:21:37 +0100

    Factored pixel shader shadow mapping code

--- Sources/Directional Shadow Mapping/PixelShader.hlsl
+++ Sources/Directional Shadow Mapping/PixelShader.hlsl
@@ -3,6 +3,37 @@
 static const float SMAP_SIZE = 4096.0f;
 static const float SMAP_DX = 1.0f / SMAP_SIZE;
 
+float CalculateShadowFactor(PixelInput input)
+{
+	// Calculate shadow texture coordinates
+	float2 tex_coords;
+	tex_coords.x = +input.lightViewPosition.x / input.lightViewPosition.w / 2.0f + 0.5f;
+	tex_coords.y = -input.lightViewPosition.y / input.lightViewPosition.w / 2.0f + 0.5f;
+
+	// Calculate pixels depth
+	float pixel_depth = input.lightViewPosition.z / input.lightViewPosition.w;
+
+	// Kernel for soft shadows
+	const float dx = SMAP_DX;
+	const float2 offsets[9] =
+	{
+		float2(-dx,  -dx), float2(0.0f,  -dx), float2(dx,  -dx),
+		float2(-dx, 0.0f), float2(0.0f, 0.0f), float2(dx, 0.0f),
+		float2(-dx,  +dx), float2(0.0f,  +dx), float2(dx,  +dx)
+	};
+
+	// Sample and average shadow map for shadow factor
+	float lighting = 1.0f;
+
+	[unroll]
+	for (int i = 0; i < 9; ++i)
+	{
+		lighting += gShadowMap.SampleCmpLevelZero(gShadowSampler, tex_coords.xy + offsets[i], pixel_depth).r;
+	}
+
+	return lighting / 9;
+}
+
 float4 CalculateDirectionalLighting(float3 position, float3 normal, PixelInput input)
 {
 	float4 diffuse_light_colour = float4(0.6f, 0.6f, 0.6f, 1.0f);
@@ -13,33 +44,10 @@ float4 CalculateDirectionalLighting(float3 position, float3 normal, PixelInput i
 	float3 light_direction = cLightDirection.xyz;
 
 	// Shadow map?
-	float2 shadowTexCoords;
-	shadowTexCoords.x = input.lightViewPosition.x / input.lightViewPosition.w / 2.0f + 0.5f;
-	shadowTexCoords.y = -input.lightViewPosition.y / input.lightViewPosition.w / 2.0f + 0.5f;
-	float pixelDepth = input.lightViewPosition.z / input.lightViewPosition.w;
-
-	const float dx = SMAP_DX;
-
-
-	float lighting = 1;
-	const float2 offsets[9] =
-	{
-		float2(-dx,  -dx), float2(0.0f,  -dx), float2(dx,  -dx),
-		float2(-dx, 0.0f), float2(0.0f, 0.0f), float2(dx, 0.0f),
-		float2(-dx,  +dx), float2(0.0f,  +dx), float2(dx,  +dx)
-	};
-
-	[unroll]
-	for (int i = 0; i < 9; ++i)
-	{
-		lighting += gShadowMap.SampleCmpLevelZero(gShadowSampler, shadowTexCoords.xy + offsets[i], pixelDepth).r;
-	}
-
-	lighting /= 9;
-
+	float shadow_factor = CalculateShadowFactor(input);
 
 	// Diffuse lighting
-	float4 diffuse_light = saturate(dot(-light_direction, normal)) * diffuse_light_colour * lighting;
+	float4 diffuse_light = saturate(dot(-light_direction, normal)) * diffuse_light_colour * shadow_factor;
 
 	// Ambient lighting
 	float4 ambient_light = ambient_light_colour;
@@ -49,7 +57,7 @@ float4 CalculateDirectionalLighting(float3 position, float3 normal, PixelInput i
 	float3 reflect_direction = reflect(light_direction, normal);
 
 	float specular_factor = pow(max(dot(view_direction, reflect_direction), 0.0), 16.0f);
-	float4 specular_light = float4(specular_factor * specular_light_colour) * lighting;
+	float4 specular_light = float4(specular_factor * specular_light_colour) * shadow_factor;
 
 	// Combine the lights
 	return diffuse_light + ambient_light + specular_light;
@@ -61,8 +69,8 @@ float4 main(PixelInput input) : SV_TARGET
 	// Interpolating normal can unnormalize it, so normalize it.
 	input.normal = normalize(input.normal);
 
-// Calculate directional light
-float4 light_colour = CalculateDirectionalLighting(input.position.xyz, input.normal, input);
+	// Calculate directional light
+	float4 light_colour = CalculateDirectionalLighting(input.position.xyz, input.normal, input);
 
-return light_colour;
+	return light_colour;
 }
