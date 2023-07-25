8ae003bc16f9421383b07a57f7895f84a60ff13f
Callum Anning <Callum5042@gmail.com>
2023-07-25 13:57:26 +0100

    Updated pixel shader to output it as greyscale

--- Sources/Directional Shadow Mapping/OverlayPixelShader.hlsl
+++ Sources/Directional Shadow Mapping/OverlayPixelShader.hlsl
@@ -3,5 +3,5 @@
 float4 main(PixelInput input) : SV_TARGET
 {
     float4 diffuse_texture = gTextureDiffuse.Sample(gSamplerAnisotropic, input.tex);
-    return diffuse_texture;
+    return diffuse_texture.rrrr;
 }
